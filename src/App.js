0f6dd722ce326881f75fb1f5023f15639a866ff6
Tingting-Jiang <ziwangzhu@Ziwangs-iMac.local>
2022-08-16 21:59:02 -0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './App.css';
 import HomePage from "./Components";
+import "./vendors/bootstrap/css/bootstrap.min.css";
+// import "./vendors/bootstrap/bootstrap.min.css";
 
 function App() {
   return (
-    <HomePage/>
+    <div className="container">
+      <HomePage/>
+    </div>
   );
 }
 
