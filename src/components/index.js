67d9855b2136a8112ece957b3396e33a5f0315bb
Tingting-Jiang <ziwangzhu@Ziwangs-iMac.local>
2022-08-16 22:32:09 -0700

    udpate version

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
     return (
       <>
-       This is the home page v3
+       This is the home page v4
 
         <video>
             <source type="video/mp4" src="../vendors/test_video.mp4" />
