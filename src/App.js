4fc151e6e2d380b7c14fa102f86d8223b6c4521c
Tingting-Jiang <ziwangzhu@Ziwangs-iMac.local>
2022-08-16 22:26:17 -0700

    add fontawesome

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from "./Components";
 import "./vendors/bootstrap/css/bootstrap.min.css";
 // import "./vendors/bootstrap/bootstrap.min.css";
+import "./vendors/fontawesome/css/all.min.css";
 
 function App() {
   return (
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,22 @@ import React from "react";
 const HomePage = () => {
     return (
       <>
-       This is the home page v2
+       This is the home page v3
 
         <video>
             <source type="video/mp4" src="../vendors/test_video.mp4" />
         </video>
+        <div className="row text center">
+            <div class="col">
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+        </div>
       </>
     );
   };
